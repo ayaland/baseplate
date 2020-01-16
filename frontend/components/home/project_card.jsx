26e4aa0050e6097cfc661f785a04311ffd5d5b20
ayaland <ashirai@gmail.com>
2020-01-15 17:04:14 -0800

    Add render of add to project add project card

--- frontend/components/home/project_card.jsx
+++ frontend/components/home/project_card.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 
-class ProjectCard extends React.Component {
-    render() {
+function ProjectCard(props) {
         return (
-            <main>
                 <article className="project-card">
                     <a href="/" className="card_link">
                         <div className="card_content">
-                            <h2 className="card_title flush">{this.props.project.name}</h2>
-                            <p className="card_description flush">{this.props.title}</p>
+                            <h2 className="card_title flush">{props.project.name}</h2>
+                            <p className="card_description flush">{props.title}</p>
                             <img className="card_avatar" src={window.demo_avatar} />
                         </div>
                     </a>
                 </article>
-            </main>
         );
-    }
-
 }
 
-const mapStateToProps = (state) => {
-    console.log(state);
-    return state;
-    // return {
-    //     currentUser: users[session.id]
-    // };
-};
-
-export default connect(mapStateToProps)(ProjectCard);
+export default ProjectCard;
